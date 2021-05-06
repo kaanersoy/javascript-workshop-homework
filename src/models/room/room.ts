ba00bfa20e0fd65b9064952675980e5ed9195874
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-06 21:53:45 +0300

    Add allRoom & getRoom methods for room.ts

--- src/models/room/room.ts
+++ src/models/room/room.ts
@@ -3,17 +3,26 @@ export class Room {
   name: string;
   members: User[]
   round: number
-  constructor (name:string) {
+  roundScores?: []
+
+  constructor (name: string) {
     this.name = name
     this.members = []
     this.round = 1
+    Room.allRoomInstances.push(this)
   }
 
+  static allRoomInstances: Room[] = []
   static allRooms () {
+    return this.allRoomInstances
+  }
 
+  static getRoom (name: string) {
+    return Room.allRoomInstances.find(room => room.name === name)
   }
 
   join (user: User) {
+    user.room = this
     this.members.push(user)
     // If someone in the room
     if (this.members[0] === user) {
@@ -31,5 +40,19 @@ export class Room {
 
     // If user not the host
     return false
+  }
+
+  nextRound (user: User) {
+    // If user is host
+    if (this.members[0] === user) {
+      this.round++
+      return true
+    }
+    // If user is not host
+    return false
+  }
+
+  smile (user: User) {
+    return `${user.name} smiled`
   }
 }
